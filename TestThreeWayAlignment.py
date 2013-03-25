730095d50bbb8e006312e357d80bd3271a5d9b46
Dan Schellhas <dan_schellhas@hotmail.com>
2013-03-25 18:04:36 -0400

    ThreeWay alignments complete
    A test of circular threeway alignments. The function
    ThreeWayAlignment(c1, c2, c3) is in ThreeWayAlignment.py.
    TestThreeWayAlignment.py shows the alignments both good and bad and
    gives some minor stats about them.

--- TestThreeWayAlignment.py
+++ TestThreeWayAlignment.py
@@ -1,7 +1,33 @@
 # This program combines three sets of correspondences into a 3-way alignment
+from ReadCorrespondences import ReadCorrespondences
+from ThreeWayAlignment import ThreeWayAlignment
 
 correspondences1, header1, error1 = ReadCorrespondences('2QBG_3U5H_correspondences_511bdafa59aa6.txt',0)
 correspondences2, header2, error2 = ReadCorrespondences('3V2F_3U5H_correspondences_511bdbc4dc920.txt',1)
 correspondences3, header3, error3 = ReadCorrespondences('2QBG_3V2F_correspondences_50dd09ef4f614.txt',1)
 
 threeway, twoway = ThreeWayAlignment(correspondences1,correspondences2,correspondences3)
+
+print "Circular alignments:"
+print
+for threewayAlign in threeway:
+	print "(" + threewayAlign[0] + ", " + threewayAlign[1] + ", " + threewayAlign[2] + ")"
+
+print
+print "Broken alignments:"
+print
+for twowayAlign in twoway:
+	print "(" + twowayAlign[0] + ", " + twowayAlign[1] + ")"
+
+# Count the good and bad alignments. Each bad alignment produces two elements	
+good = len(threeway)
+bad = len(twoway)
+print
+print "Total circular alignments: " + str(good)
+print "Total broken alignments: " + str(bad)
+
+# Make them no longer integers so we get a good decimal. Divide broken alignments by three
+# for fair comparison to circular alignments.
+good = good * 1.0
+bad = bad / 3.0
+print "Quality: " + ("%0.2f" % (100 * good / (good + bad))) + "%"
--- ThreeWayAlignment.py
+++ ThreeWayAlignment.py
@@ -4,3 +4,50 @@
 # OUTPUT: All pairwise correspondences that make consistent triples
 #         Plus all pairwise correspondences that are not part of a consistent triple
 
+def ThreeWayAlignment(c1, c2, c3):
+	threeWay = []
+	twoWay = []
+	c2used = []
+	c3used = []
+	for itemSource, itemTarget in c1.iteritems():
+		if (c2.has_key(itemTarget)):
+			if (c3.has_key(c2[itemTarget])):
+				if (c3[c2[itemTarget]] == itemSource):
+					# We have a circular match!
+					threeWay.append([itemSource, itemTarget, c2[itemTarget]])
+					c2used.append(itemTarget)
+					c3used.append(c2[itemTarget])
+				else:
+					# Target from Correspondence 3 does not match
+					twoWay.append([itemSource, itemTarget])
+					twoWay.append([itemTarget, c2[itemTarget]])
+					twoWay.append([c2[itemTarget], c3[c2[itemTarget]]])
+					c2used.append(itemTarget)
+					c3used.append(c2[itemTarget])
+			else:
+				#Target in Correspondence 3 does not exist
+				twoWay.append([itemSource, itemTarget])
+				twoWay.append([itemTarget, c2[itemTarget]])
+				c2used.append(itemTarget)
+		else:	
+			# Target in Correspondence 2 does not exist
+			twoWay.append([itemSource, itemTarget])
+	
+	# Clear used items out of the correspondences
+	for used in c2used:
+		del c2[used]
+	for used in c3used:
+		del c3[used]
+	
+	# Add in broken items that cannot be reached from Correspondence 1
+	for itemSource, itemTarget in c2.iteritems():
+		twoWay.append([itemSource, itemTarget])
+	
+	for itemSource, itemTarget in c3.iteritems():
+		twoWay.append([itemSource, itemTarget])
+		
+	return threeWay, twoWay
+	
+
+
+		
