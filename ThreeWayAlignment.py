7ac52248950d90464bb8a26efa4defd231294a23
Craig L. Zirbel <zirbel@bgnet.bgsu.edu>
2013-03-25 17:13:43 -0400

    Stub for making a 3-way alignment

---
+++ ThreeWayAlignment.py
@@ -0,0 +1,6 @@
+# This program combines three sets of correspondences into a 3-way alignment
+
+# INPUT:  Three 2-way correspondences
+# OUTPUT: All pairwise correspondences that make consistent triples
+#         Plus all pairwise correspondences that are not part of a consistent triple
+
