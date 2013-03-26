f234df21b4331abe2b320aa0e342aa7defc60dd0
Craig L. Zirbel <zirbel@bgnet.bgsu.edu>
2013-03-25 22:11:09 -0400

    Added TestRemoveGaps

---
+++ TestRemoveGaps.py
@@ -0,0 +1,14 @@
+# TestRemoveGaps tests the ability of RemoveGaps to leave out all-gap columns of an alignment
+
+from RemoveGaps import RemoveGaps
+
+# -------- First test:  remove the columns from this very small alignment
+
+seq[0] = 'AG---CCU-G-A'
+seq[1] = '-G--UCUC-A-A'
+seq[2] = 'UG---UCC-G-A'
+
+newseq = RemoveGaps(seq)
+
+print newseq
+
