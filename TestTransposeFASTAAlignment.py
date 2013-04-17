79e112e27b669beb52d1f181f9205fb49a17b3fa
Ying-Ju Chen <yingc@bgsu.edu>
2013-04-16 20:56:29 -0400

    add an outputfile for the TransposeFASTAAlignment

--- TestTransposeFASTAAlignment.py
+++ TestTransposeFASTAAlignment.py
@@ -4,7 +4,7 @@ from TransposeFASTAAlignment import TransposeFASTAAlignment
 
 # ------------ Test 1
 
-numcolumns, numlines = TransposeFASTAAlignment("SSU_GG_MSA_escherichia_coli.fasta")
+numcolumns, numlines = TransposeFASTAAlignment("SSU_GG_MSA_escherichia_coli.fasta", "SSU_GG_MSA_escherichia_coli_transpose.txt")
 
 print numcolumns
 print numlines
